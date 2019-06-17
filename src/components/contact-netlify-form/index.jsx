32fb06a91c59ff3ae83c18ec06d82451915f47e1
rtre84 <rtre84@users.noreply.github.com>
2019-06-17 21:33:46 +0530

    Added netlify form component so that the contact page can be used to send me messages if needed.

---
+++ src/components/contact-netlify-form/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import Icon from '../icon';
+import OutboundLink from '../outbound-link';
+
+import styles from './styles.module.css';
+
+const ContactNetlifyForm = ({ title }) => (
+  <div className={styles.contact}>
+    <h1>{title || 'Messages make the world go round.'}</h1>
+    <p className={styles.email}>
+      Reach out and say hello.
+      <br />
+    </p>
+
+    <form name="serving-niches-form" method="POST" data-netlify-recaptcha="true" data-netlify="true" className={styles['contact-form']}>
+      <p>
+        <label>Email:</label>
+        <input className="form-control" type="text" name="name" />
+      </p>
+      <p>
+        <label>Message:</label>
+        <textarea className="form-control textarea" name="message" />
+      </p>
+      <div data-netlify-recaptcha="true" />
+      <button className="square-button form-control" type="submit">Send</button>
+    </form>
+
+    <div className={styles.lineBreak} />
+    <ul className={styles.social}>
+      <li>
+        <OutboundLink to="https://github.com/servingniches/">
+          <Icon name="github" />
+        </OutboundLink>
+      </li>
+      <li>
+        <OutboundLink to="https://www.linkedin.com/in/anupvasudevan">
+          <Icon name="linkedin" />
+        </OutboundLink>
+      </li>
+      <li>
+        <OutboundLink to="https://twitter.com/thechronicler/">
+          <Icon name="twitter" />
+        </OutboundLink>
+      </li>
+    </ul>
+  </div>
+);
+
+export default ContactNetlifyForm;
