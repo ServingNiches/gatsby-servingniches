ea0e1c376da93561dccdbf9b2af5451675f880f7
rtre84 <rtre84@users.noreply.github.com>
2019-06-20 22:22:45 +0530

    Added analytics account data for GA and Segment.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXXXXXX-X',
+        trackingId: 'UA-141909743-1',
       },
     },
     {
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -10,7 +10,6 @@ const ContactPage = ({ location }) => (
   <ContentPage>
     <Meta title="Contact" description="If you have an upcoming project you'd like to talk about or are looking to collaborate and build something special, say hello and send an email." location={location} />
     <Wrapper>
-      {/*<Contact />*/}
       <ContactNetlifyForm />
     </Wrapper>
   </ContentPage>
