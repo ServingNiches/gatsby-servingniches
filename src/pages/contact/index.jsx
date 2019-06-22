affccc57aca86adb46e2b06e0a5e538e4db37003
rtre84 <rtre84@users.noreply.github.com>
2019-06-22 15:31:37 +0530

    Typo in location.href resulted in form not rendering. location.href returns the full url.

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -11,7 +11,7 @@ const ContactPage = ({ location }) => (
     <Meta title="Contact" description="If you have an upcoming project you'd like to talk about or are looking to collaborate and build something special, send me an email." location={location} />
     <Wrapper>
       {/* If the url is servingniches.org, then present the netlify form  */}
-      { location.href === 'https://servingniches.org/' ? (
+      { location.href === 'https://servingniches.org/contact/' ? (
         <ContactNetlifyForm />
       ) : (
         <Contact />
