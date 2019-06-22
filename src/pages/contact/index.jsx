d90ceae41ccdc2a538a1f3094c780da218d2e549
rtre84 <rtre84@users.noreply.github.com>
2019-06-22 15:18:29 +0530

    Added code to render contact form only if the site is being served via netlify. Netlify form won't work without it.

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -8,9 +8,14 @@ import Wrapper from '../../components/wrapper';
 
 const ContactPage = ({ location }) => (
   <ContentPage>
-    <Meta title="Contact" description="If you have an upcoming project you'd like to talk about or are looking to collaborate and build something special, say hello and send an email." location={location} />
+    <Meta title="Contact" description="If you have an upcoming project you'd like to talk about or are looking to collaborate and build something special, send me an email." location={location} />
     <Wrapper>
-      <ContactNetlifyForm />
+      {/* If the url is servingniches.org, then present the netlify form  */}
+      { location.href === 'https://servingniches.org/' ? (
+        <ContactNetlifyForm />
+      ) : (
+        <Contact />
+      )}
     </Wrapper>
   </ContentPage>
 );
